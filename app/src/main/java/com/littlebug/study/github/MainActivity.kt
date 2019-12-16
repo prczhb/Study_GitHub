4ce702b442d2b5a7daea3928ba2626f7aa55ec3a
prczhb <prczhb@gmail.com>
2019-12-16 21:03:40 +0800

    update: dev_1 的第二次修改

--- app/src/main/java/com/littlebug/study/github/MainActivity.kt
+++ app/src/main/java/com/littlebug/study/github/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("dev_1 的修改")
+        println("dev_1 的第二次修改")
     }
 }
