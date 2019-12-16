625e2bf64bc3b5d19c6824ed3831cd224090107b
prczhb <prczhb@gmail.com>
2019-12-16 20:49:33 +0800

    branch: dev1

--- app/src/main/java/com/littlebug/study/github/MainActivity.kt
+++ app/src/main/java/com/littlebug/study/github/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("dev_1 的修改")
     }
 }
